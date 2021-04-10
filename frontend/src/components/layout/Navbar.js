4754ed1419889513c1d113add8bf8aebed643140
Ankit Pathak <ankit.pathak.1500@gmail.com>
2021-04-10 10:06:50 +0530

    frontend navbar

---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../style.css'
+ const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                
+                <ul id = "nav">
+                <li> <a href = "profile.html"> Home</a></li>
+                    <li> <a href = "profiles.html"> profiles</a></li>
+                    <li> <a href = "login.html"> login</a></li>
+                    <li> <a href = "registration.html">register</a></li>
+                    
+                </ul>
+
+                    
+            </nav>
+        </div>
+    )
+}
+export default Navbar
