05f662854b09c2d5bc53a81f25ca3b1a754de9a6
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-04-10 11:52:38 +0530

    functions done

--- backend/contracts/Functions.sol
+++ backend/contracts/Functions.sol
@@ -205,3 +205,452 @@ contract Functions is Global {
             }
         }
     }
+
+     function remove(uint256 index, bytes32 id) internal {
+        if (index >= lenders[id].clients.length) return;
+
+        lenders[id].clients[index] = lenders[id].clients[
+            lenders[id].clients.length - 1
+        ];
+        lenders[id].amount[index] = lenders[id].amount[
+            lenders[id].months.length - 1
+        ];
+        lenders[id].months[index] = lenders[id].months[
+            lenders[id].amount.length - 1
+        ];
+        lenders[id].clients.pop();
+        lenders[id].months.pop();
+        lenders[id].amount.pop();
+    }
+
+    function monthly() external {
+        for (uint256 i = 0; i < decentralizedBank.users.length; i++) {
+            if (users[decentralizedBank.users[i]].isUser) {
+                bytes32 id = decentralizedBank.users[i];
+                if (lenders[id].islender) {
+                    for (uint256 j = 0; j < lenders[id].clients.length; j++) {
+                        lenders[id].months[j]--;
+                        if (lenders[id].months[j] == 0) {
+                            if (
+                                users[lenders[id].clients[j]].inhand >=
+                                lenders[id].amount[j]
+                            ) {
+                                transaction_count++;
+                                bytes32 tid =
+                                    stringToBytes32(
+                                        string(
+                                            abi.encodePacked(
+                                                "loan",
+                                                uint2str(transaction_count),
+                                                "_1"
+                                            )
+                                        )
+                                    );
+                                users[lenders[id].clients[j]].inhand -= lenders[
+                                    id
+                                ]
+                                    .amount[j];
+                                users[lenders[id].clients[j]].income -= lenders[
+                                    id
+                                ]
+                                    .amount[j];
+                                users[id].inhand += lenders[id].amount[j];
+                                users[id].income += lenders[id].amount[j];
+                                users[lenders[id].clients[j]].transactions.push(
+                                    tid
+                                );
+                                users[id].transactions.push(tid);
+                                decentralizedBank.transactions.push(tid);
+                                remove(j, id);
+                                j--;
+                            }
+                        } else if (lenders[id].months[j] < 0) {
+                            users[id].net_worth +=
+                                (lenders[id].amount[j] *
+                                    (lenders[id].ipm + lenders[id].iipm * -1)) /
+                                100;
+                            users[lenders[id].clients[j]].net_worth -=
+                                (lenders[id].amount[j] *
+                                    (lenders[id].ipm + lenders[id].iipm * -1)) /
+                                100;
+                            lenders[id].amount[j] +=
+                                (lenders[id].amount[j] *
+                                    (lenders[id].ipm + lenders[id].iipm * -1)) /
+                                100;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (uint256 i = 0; i < decentralizedBank.companies.length; i++) {
+            if (companies[decentralizedBank.companies[i]].isCompany) {
+                int256 amount =
+                    companies[decentralizedBank.companies[i]].salary;
+                for (
+                    uint256 j = 0;
+                    j <
+                    companies[decentralizedBank.companies[i]].employees.length;
+                    j++
+                ) {
+                    transaction_count++;
+                    bytes32 tid =
+                        stringToBytes32(
+                            string(
+                                abi.encodePacked(
+                                    "salary",
+                                    uint2str(transaction_count),
+                                    "_0"
+                                )
+                            )
+                        );
+                    Transaction memory newtransaction =
+                        Transaction(
+                            tid,
+                            true,
+                            decentralizedBank.companies[i],
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ],
+                            amount,
+                            false,
+                            ""
+                        );
+                    if (
+                        companies[decentralizedBank.companies[i]].inhand >=
+                        amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j]
+                    ) {
+                        newtransaction.success = true;
+                        newtransaction.summary = "Salary Credited Successfully";
+                        decentralizedBank.transactions.push(tid);
+                        companies[decentralizedBank.companies[i]]
+                            .transactions
+                            .push(tid);
+                        users[
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ]
+                        ]
+                            .transactions
+                            .push(tid);
+                        companies[decentralizedBank.companies[i]].inhand -=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                        companies[decentralizedBank.companies[i]].income -=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                        //companies[decentralizedBank.companies[i]].net_worth-=amount+companies[decentralizedBank.companies[i]].remaining_salary[j];
+                        users[
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ]
+                        ]
+                            .net_worth +=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                        users[
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ]
+                        ]
+                            .income +=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                        users[
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ]
+                        ]
+                            .inhand +=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                        companies[decentralizedBank.companies[i]]
+                            .remaining_salary[j] = 0;
+                    } else {
+                        newtransaction.summary = "Unable to Credit Salary";
+                        decentralizedBank.transactions.push(tid);
+                        companies[decentralizedBank.companies[i]]
+                            .transactions
+                            .push(tid);
+                        users[
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ]
+                        ]
+                            .transactions
+                            .push(tid);
+                        users[
+                            companies[decentralizedBank.companies[i]].employees[
+                                j
+                            ]
+                        ]
+                            .net_worth +=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                        companies[decentralizedBank.companies[i]]
+                            .remaining_salary[j] +=
+                            amount +
+                            companies[decentralizedBank.companies[i]]
+                                .remaining_salary[j];
+                    }
+                    transactions[tid] = newtransaction;
+                }
+            }
+        }
+    }
+
+    function productPurchase(
+        bytes32 fromid,
+        bytes32 productId,
+        int256 quantity
+    ) external {
+        if (
+            companies[products[productId].company].isCompany &&
+            users[fromid].isUser &&
+            products[productId].isProduct
+        ) {
+            transaction_count++;
+            bytes32 tid =
+                stringToBytes32(
+                    string(
+                        abi.encodePacked(
+                            "product_purchase",
+                            uint2str(transaction_count),
+                            "_0"
+                        )
+                    )
+                );
+            //Product memory tempProduct = products[productId];
+            //require((products[productId].selling_price)*quantity <= users[fromid].inhand, "Insufficient Balance.");
+            int256 amount = products[productId].selling_price * quantity;
+            Transaction memory newtransaction =
+                Transaction(
+                    tid,
+                    true,
+                    fromid,
+                    products[productId].company,
+                    amount,
+                    false,
+                    ""
+                );
+            if (
+                products[productId].selling_price * quantity <=
+                users[fromid].inhand
+            ) {
+                newtransaction.success = true;
+                newtransaction.summary = "Transaction carried Successfully";
+
+                companies[products[productId].company].net_worth +=
+                    (products[productId].selling_price) *
+                    quantity;
+                companies[products[productId].company].income +=
+                    (products[productId].selling_price -
+                        products[productId].cost_price) *
+                    quantity;
+                decentralizedBank.transactions.push(tid);
+                companies[products[productId].company].inhand +=
+                    (products[productId].selling_price) *
+                    quantity;
+                companies[products[productId].company].transactions.push(tid);
+                users[fromid].net_worth -=
+                    (products[productId].selling_price -
+                        products[productId].cost_price) *
+                    quantity;
+                users[fromid].inhand -=
+                    (products[productId].selling_price) *
+                    quantity;
+                products[productId].quantity -= quantity;
+                users[fromid].transactions.push(tid);
+            } else {
+                newtransaction.summary = "Insufficient Balance";
+                companies[products[productId].company].transactions.push(tid);
+                decentralizedBank.transactions.push(tid);
+                users[fromid].transactions.push(tid);
+            }
+            transactions[tid] = newtransaction;
+        }
+    }
+
+    function becomeLender(
+        bytes32 id,
+        int256 ipm,
+        int256 iipm
+    ) external {
+        if (users[id].isUser && users[id].inhand >= 1000000) {
+            lenders[id] = Lender({
+                id: id,
+                islender: true,
+                clients: new bytes32[](0),
+                ipm: ipm,
+                iipm: iipm,
+                months: new int256[](0),
+                amount: new int256[](0)
+            });
+        }
+    }
+
+    function takeloan(
+        bytes32 fromid,
+        bytes32 lenderid,
+        int256 amount,
+        int256 month
+    ) external {
+        if (users[fromid].isUser && lenders[lenderid].islender) {
+            transaction_count++;
+            bytes32 tid =
+                stringToBytes32(
+                    string(
+                        abi.encodePacked(
+                            "loan",
+                            uint2str(transaction_count),
+                            "_0"
+                        )
+                    )
+                );
+            Transaction memory newtransaction =
+                Transaction(tid, true, lenderid, fromid, amount, false, "");
+            users[lenderid].transactions.push(tid);
+            users[fromid].transactions.push(tid);
+            decentralizedBank.transactions.push(tid);
+            users[lenderid].inhand -= amount;
+            users[fromid].inhand += amount;
+            users[lenderid].net_worth +=
+                ((amount * lenders[lenderid].ipm) / 100) *
+                month;
+            users[fromid].net_worth -=
+                ((amount * lenders[lenderid].ipm) / 100) *
+                month;
+            lenders[lenderid].clients.push(fromid);
+            lenders[lenderid].months.push(month);
+            lenders[lenderid].amount.push(
+                amount + ((amount * lenders[lenderid].ipm) / 100) * month
+            );
+            transactions[tid] = newtransaction;
+        }
+    }
+
+    function createProduct(
+        bytes32 companyId,
+        int256 cost_price,
+        int256 selling_price,
+        int256 quantity
+    ) external {
+        if (
+            companies[companyId].isCompany &&
+            cost_price * quantity <= companies[companyId].inhand
+        ) {
+            products_count += 1;
+            bytes32 productId =
+                stringToBytes32(
+                    string(
+                        abi.encodePacked("product_", uint2str(products_count))
+                    )
+                );
+            Product memory newProduct =
+                Product(
+                    productId,
+                    true,
+                    companyId,
+                    cost_price,
+                    selling_price,
+                    quantity
+                );
+            products[productId] = newProduct;
+            companies[companyId].products.push(productId);
+            companies[companyId].inhand -= cost_price * quantity;
+            companies[companyId].net_worth -= cost_price * quantity;
+            //companies[companyId].income-=cost_price*quantity;
+            decentralizedBank.products.push(productId);
+        }
+    }
+
+    function changeProductDetails(
+        bytes32 productId,
+        int256 cost_price,
+        int256 selling_price
+    ) external {
+        if (products[productId].isProduct) {
+            if (cost_price > 0) {
+                products[productId].cost_price = cost_price;
+            }
+
+            if (selling_price > 0) {
+                products[productId].selling_price = selling_price;
+            }
+        }
+    }
+
+    function createVacancy(
+        bytes32 companyId,
+        int256 vacancy,
+        int256 salary
+    ) external {
+        if (companies[companyId].isCompany) {
+            companies[companyId].vacancy = vacancy;
+            companies[companyId].salary = salary;
+        }
+    }
+
+    function recruitEmployee(bytes32 companyId, bytes32 userId) external {
+        if (
+            companies[companyId].isCompany && companies[companyId].vacancy > 0
+        ) {
+            companies[companyId].employees.push(userId);
+            companies[companyId].remaining_salary.push(0);
+            companies[companyId].vacancy -= 1;
+            users[userId].companies.push(companyId);
+        }
+    }
+
+    function userToUserTransaction(
+        bytes32 fromid,
+        bytes32 toid,
+        int256 amount
+    ) external {
+        if (users[fromid].isUser && users[toid].isUser) {
+            transaction_count++;
+            // string memory description = "u2u";
+            // string memory transaction_type = "_2";
+            bytes32 tid =
+                stringToBytes32(
+                    string(
+                        abi.encodePacked(
+                            "u2u",
+                            uint2str(transaction_count),
+                            "_2"
+                        )
+                    )
+                );
+            Transaction memory newtransaction =
+                Transaction(
+                    tid,
+                    true,
+                    fromid,
+                    toid,
+                    amount,
+                    false,
+                    "Insufficient balance"
+                );
+            if (users[fromid].inhand >= amount) {
+                users[toid].net_worth += amount;
+                users[toid].inhand += amount;
+                users[fromid].net_worth -= amount;
+                users[fromid].inhand -= amount;
+                newtransaction.success = true;
+                newtransaction.summary = "Transaction Successfull";
+            }
+            users[toid].transactions.push(tid);
+            users[fromid].transactions.push(tid);
+            decentralizedBank.transactions.push(tid);
+            transactions[tid] = newtransaction;
+        }
+    }
+}
