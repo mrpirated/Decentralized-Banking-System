480265eb132063c1607cd41d58c4162831d62893
Aditya Darji <darjiaditya2000@gmail.com>
2021-04-11 04:57:24 +0530

    take loan minor changes

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -16,7 +16,7 @@ router.post("/productPurchase", productPurchase);
 router.post("/becomeLender", becomeLender);
 router.post("/takeloan", takeloan);
 router.post("/userToUserTransaction", userToUserTransaction);
-router.get("/getlenders", getLenders);
+router.get("/getLenders", getLenders);
 router.get("/getUserTransactions", getUserTransactions);
 router.get("/getUserInfo", getUserInfo);
 
